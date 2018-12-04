c8f6a029587f14d23c0015a5b5b6b19569d196a6
sfhorng <32020036+sfhorng@users.noreply.github.com>
2018-12-03 20:56:36 -0500

    changed some stuff

--- flask-nante/weather.py
+++ flask-nante/weather.py
@@ -33,7 +33,7 @@ def getWeather():
         tempC = kelvinToCelcius(temperature)
         tempF = kelvinToFahrenheit(temperature)
         humidity = json_data["main"]["humidity"]
-        cursor.execute("INSERT INTO WEATHER VALUES (%s, %s, %s, %s, %s)", (city, description, tempC, tempF, humidity))
+        cursor.execute("INSERT INTO WEATHER VALUES (%s, %s, %s, %s, %s)", (city, summary, tempc, tempf, humidity))
 
     connect.commit()
     cursor.close()
