7b3c9ef1b8ba8a80945b9a32bf15c524b1dfce84
algeary <34697133+algeary@users.noreply.github.com>
2018-12-03 20:52:44 -0500

    changes to make data updates dynamic

--- flask-nante/nanteapp.py
+++ flask-nante/nanteapp.py
@@ -1,7 +1,16 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import modelsNew
 import forms
+import AllMoviesParse
+import ParseHotels
+import weather
 
 app = Flask(__name__)
 app.secret_key = 's3cr3t'
@@ -10,19 +19,19 @@ db = SQLAlchemy(app, session_options={'autocommit': False})
 
 @app.route('/')
 def main():
+    weather.getWeather()
+    AllMoviesParse.getHTML()
+    ParseHotels.getHTML()
     return render_template('homepage.html')
 
 @app.route('/city/<city>')
 def City(city):
-    CityMovies = db.session.query(modelsNew.Movies)\
-        .filter(modelsNew.Movies.city == city).all()
-    CityWeather = db.session.query(modelsNew.Weather)\
-        .filter(modelsNew.Weather.city == city).all()
-    CityHotels = db.session.query(modelsNew.Hotel)\
-        .filter(modelsNew.Hotel.city == city).all()
-    CityRestaurants = db.session.query(modelsNew.Restaurant)\
-        .filter(modelsNew.Restaurant.city == city).all()
-    return render_template('city.html', movies=CityMovies, weather=CityWeather, hotels = CityHotels, restaurants = CityRestaurants)
+    CityMovies = db.session.query(modelsNew.Movies)        .filter(modelsNew.Movies.city == city).all()
+    CityWeather = db.session.query(modelsNew.Weather)        .filter(modelsNew.Weather.city == city).all()
+    CityHotels = db.session.query(modelsNew.Hotel)        .filter(modelsNew.Hotel.city == city).all()
+    CityRestaurants = db.session.query(modelsNew.Restaurant)        .filter(modelsNew.Restaurant.city == city).all()
+    return render_template('city.html', movies=CityMovies, weather=CityWeather, hotel = CityHotels, restaurants = CityRestaurants)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
